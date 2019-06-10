6ef5d8a5ea468eaa4513b419f434e80b04b0c759
Phil Davidson <phil_davidson@hotmail.com>
2019-06-10 14:19:11 +1200

    Update image-resize.py
    removed code to create bucket if it doesn't exist
    assumes that the xxx-resized100 bucket exists
    added extra debugging

--- example-3-create-a-deployment-package/image-resize.py
+++ example-3-create-a-deployment-package/image-resize.py
@@ -1,6 +1,7 @@
 import boto3
 from wand.image import Image
 import json
+import os
 
 
 s3 = boto3.client('s3')
@@ -9,27 +10,35 @@ s3 = boto3.client('s3')
 def lambda_handler(event, context):
     bucketname=(event["Records"][0]["s3"]["bucket"]["name"])
     key= (event["Records"][0]["s3"]["object"]["key"])
+    
     print(event["Records"][0]["s3"]["bucket"]["name"])
+    print ("my incoming bucketname = ", bucketname)
+    print ("my incoming key = ", key)
+
     with open('/tmp/image.jpg', 'wb') as data:
         s3.download_fileobj(bucketname, key, data)
-
 
     with Image(filename='/tmp/image.jpg') as img:
         print('width =', img.width)
         print('height =', img.height)
         img.resize(200, 200)
 
-
         #this will save the resized file
         img.save(filename='/tmp/image.jpg')
 
 
-    resizeName=key
+    resizeName="resized-"+key
     upload = boto3.resource('s3')
+    uploadbucketname = bucketname+"-resized100"
+
+    print("my uploadbucketname = ", uploadbucketname)
+    print("my resizename = ", resizeName)
+    print("uploading the resized image...")
+    
     try:
-        upload.Object(bucketname+"-resized100", resizeName).upload_file('/tmp/image.jpg')
-
-    except:
-        s3.create_bucket(Bucket=bucketname+"-resized100")
-        upload.Object(bucketname+"-resized100", resizeName).upload_file('/tmp/image.jpg')
-
+        upload.Object(uploadbucketname, resizeName).upload_file('/tmp/image.jpg')
+        print("uploading succeeded")
+    except Exception as e:
+        print ("uploading failed")
+        print ("%s", e)
+    
